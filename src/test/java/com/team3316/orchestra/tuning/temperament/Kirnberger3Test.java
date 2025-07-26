1eaa19ec9f75cc45653940ddd4bb1653c27288cb
Jonathan Dafna <81yonatand4@dalet.school>
2025-07-27 00:57:59 +0300

    Some tuning tests

---
+++ src/test/java/com/team3316/orchestra/tuning/temperament/Kirnberger3Test.java
@@ -0,0 +1,43 @@
+package com.team3316.orchestra.tuning.temperament;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.team3316.orchestra.pitch.Accidental;
+import com.team3316.orchestra.pitch.Diatonic;
+import com.team3316.orchestra.pitch.NamedNote;
+import com.team3316.orchestra.tuning.TuningSystem;
+
+public class Kirnberger3Test {
+    @Test
+    @DisplayName("BWV 847/2")
+    void melody() {
+        final TuningSystem sys = new Kirnberger3();
+        final NamedNote[] notes = {
+            NamedNote.of(Diatonic.C, 2),
+            NamedNote.of(Diatonic.B, 1),
+            NamedNote.of(Diatonic.C, 2),
+            NamedNote.of(Diatonic.G, 1),
+            NamedNote.of(Diatonic.A, Accidental.FLAT, 1),
+            NamedNote.of(Diatonic.C, 2),
+            NamedNote.of(Diatonic.B, 1),
+            NamedNote.of(Diatonic.C, 2),
+            NamedNote.of(Diatonic.D, 2),
+            NamedNote.of(Diatonic.G, 1),
+            NamedNote.of(Diatonic.C, 2),
+            NamedNote.of(Diatonic.B, 1),
+            NamedNote.of(Diatonic.C, 2),
+            NamedNote.of(Diatonic.D, 2),
+            NamedNote.of(Diatonic.F, 1),
+            NamedNote.of(Diatonic.G, 1),
+            NamedNote.of(Diatonic.A, Accidental.FLAT, 1),
+            NamedNote.of(Diatonic.G, 1),
+            NamedNote.of(Diatonic.F, 1),
+            NamedNote.of(Diatonic.E, Accidental.FLAT, 1),
+        };
+
+        for (final var nn : notes) {
+            System.out.println(sys.interpret(nn).frequency());
+        }
+    }
+}

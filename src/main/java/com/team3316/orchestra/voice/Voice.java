9e170ca2b267117462a77f6df2f708b5518186a9
Jonathan Dafna <81yonatand4@dalet.school>
2025-07-27 20:41:37 +0300

    Unused imports

--- src/main/java/com/team3316/orchestra/voice/Voice.java
+++ src/main/java/com/team3316/orchestra/voice/Voice.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import org.apache.commons.numbers.fraction.Fraction;
--- src/test/java/com/team3316/orchestra/PiecesTest.java
+++ src/test/java/com/team3316/orchestra/PiecesTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;

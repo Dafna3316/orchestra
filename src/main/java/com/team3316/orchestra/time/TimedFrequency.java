4d765b523821143cfddf5686d360a8aab5d2950f
Jonathan Dafna <81yonatand4@dalet.school>
2025-07-27 16:22:47 +0300

    Tuplets, builder from NamedNote

--- src/main/java/com/team3316/orchestra/time/TimedFrequency.java
+++ src/main/java/com/team3316/orchestra/time/TimedFrequency.java
@@ -2,13 +2,15 @@ package com.team3316.orchestra.time;
 
 import org.apache.commons.numbers.fraction.Fraction;
 
+import com.team3316.orchestra.pitch.Pitch;
+
 public record TimedFrequency(double frequency, Fraction duration) {
     public TimedFrequency {
         if (duration.compareTo(Fraction.ZERO) <= 0)
             throw new IllegalArgumentException("Negative duration");
     }
 
-    public static TimedFrequency of(TimedPitch tp) {
-        return tp.toTimedFrequency();
+    public static TimedFrequency of(Timed<Pitch> tp) {
+        return Timed.toFrequency(tp);
     }
 }
--- src/main/java/com/team3316/orchestra/time/Voice.java
+++ src/main/java/com/team3316/orchestra/time/Voice.java
@@ -3,7 +3,14 @@ package com.team3316.orchestra.time;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
+
+import org.apache.commons.numbers.fraction.Fraction;
+
+import com.team3316.orchestra.pitch.NamedNote;
+import com.team3316.orchestra.pitch.Pitch;
+import com.team3316.orchestra.tuning.TuningSystem;
 
 public record Voice(TimedFrequency[] frequencies) {
     public static Voice of(List<TimedFrequency> frequencies) {
@@ -22,11 +29,108 @@ public record Voice(TimedFrequency[] frequencies) {
         return new Builder(capacity);
     }
 
-    // MUTABLE
-    public static final class Builder {
-        private ArrayList<TimedPitch> pitches;
+    public static final class NoteBuilder implements Supplier<Voice> {
+        private ArrayList<Timed<NamedNote>> notes;
+        private TuningSystem sys;
 
-        public Builder(List<TimedPitch> pitches) {
+        public NoteBuilder(List<Timed<NamedNote>> notes, TuningSystem sys) {
+            this.notes = new ArrayList<>(notes);
+            this.sys = sys;
+        }
+
+        public NoteBuilder(int capacity, TuningSystem sys) {
+            this.notes = new ArrayList<>(capacity);
+            this.sys = sys;
+        }
+
+        public NoteBuilder(TuningSystem sys) {
+            this.notes = new ArrayList<>();
+            this.sys = sys;
+        }
+
+        public NoteBuilder add(Timed<NamedNote> note) {
+            notes.add(note);
+            return this;
+        }
+
+        public NoteBuilder add(Timed<NamedNote>[] notes) {
+            this.notes.addAll(Arrays.asList(notes));
+            return this;
+        }
+
+        public NoteBuilder add(List<Timed<NamedNote>> notes) {
+            this.notes.addAll(notes);
+            return this;
+        }
+
+        public NoteBuilder scaled(Fraction scalar, Timed<NamedNote> note) {
+            notes.add(new Timed<>(note.pitch(), note.duration().multiply(scalar)));
+            return this;
+        }
+
+        public NoteBuilder scaled(Fraction scalar, Timed<NamedNote>[] notes) {
+            this.notes.addAll(Arrays.stream(notes).map(p -> new Timed<>(p.pitch(), p.duration().multiply(scalar))).toList());
+            return this;
+        }
+
+        public NoteBuilder scaled(Fraction scalar, List<Timed<NamedNote>> notes) {
+            this.notes.addAll(notes.stream().map(p -> new Timed<>(p.pitch(), p.duration().multiply(scalar))).toList());
+            return this;
+        }
+
+        public NoteBuilder withTuningSystem(TuningSystem sys) {
+            this.sys = sys;
+            return this;
+        }
+
+        public FrequencyBuilder frequencies() {
+            return new FrequencyBuilder(notes
+                .stream()
+                .map(p -> Timed.toFrequency(p, sys))
+                .collect(Collectors.toCollection(() -> new ArrayList<>(notes.size())))
+            );
+        }
+
+        public Builder pitches() {
+            return new Builder(notes
+                .stream()
+                .map(p -> Timed.toPitch(p, sys))
+                .collect(Collectors.toCollection(() -> new ArrayList<>(notes.size())))
+            );
+        }
+
+        public Voice build() {
+            TimedFrequency[] tfs = new TimedFrequency[notes.size()];
+            int i = 0;
+            for (final var tp : notes) {
+                tfs[i++] = Timed.toFrequency(tp, sys);
+            }
+            return new Voice(tfs);
+        }
+
+        public static NoteBuilder of(List<Timed<NamedNote>> notes, TuningSystem sys) {
+            return new NoteBuilder(notes, sys);
+        }
+
+        public static NoteBuilder of(Timed<NamedNote>[] notes, TuningSystem sys) {
+            return new NoteBuilder(Arrays.asList(notes), sys);
+        }
+
+        @Override
+        public Voice get() {
+            return build();
+        }
+    }
+
+    // MUTABLE
+    public static final class Builder implements Supplier<Voice> {
+        private ArrayList<Timed<Pitch>> pitches;
+
+        Builder(ArrayList<Timed<Pitch>> pitches) {
+            this.pitches = pitches;
+        }
+
+        public Builder(List<Timed<Pitch>> pitches) {
             this.pitches = new ArrayList<>(pitches);
         }
         
@@ -38,25 +142,40 @@ public record Voice(TimedFrequency[] frequencies) {
             this.pitches = new ArrayList<>();
         }
 
-        public Builder add(TimedPitch pitch) {
+        public Builder add(Timed<Pitch> pitch) {
             pitches.add(pitch);
             return this;
         }
 
-        public Builder add(TimedPitch[] pitches) {
+        public Builder add(Timed<Pitch>[] pitches) {
             this.pitches.addAll(Arrays.asList(pitches));
             return this;
         }
 
-        public Builder add(List<TimedPitch> pitches) {
+        public Builder add(List<Timed<Pitch>> pitches) {
             this.pitches.addAll(pitches);
+            return this;
+        }
+
+        public Builder scaled(Fraction scalar, Timed<Pitch> pitch) {
+            pitches.add(new Timed<>(pitch.pitch(), pitch.duration().multiply(scalar)));
+            return this;
+        }
+
+        public Builder scaled(Fraction scalar, Timed<Pitch>[] pitches) {
+            this.pitches.addAll(Arrays.stream(pitches).map(p -> new Timed<>(p.pitch(), p.duration().multiply(scalar))).toList());
+            return this;
+        }
+
+        public Builder scaled(Fraction scalar, List<Timed<Pitch>> pitches) {
+            this.pitches.addAll(pitches.stream().map(p -> new Timed<>(p.pitch(), p.duration().multiply(scalar))).toList());
             return this;
         }
 
         public FrequencyBuilder frequencies() {
             return new FrequencyBuilder(pitches
                 .stream()
-                .map(TimedPitch::toTimedFrequency)
+                .map(Timed::toFrequency)
                 .collect(Collectors.toCollection(() -> new ArrayList<>(pitches.size())))
             );
         }
@@ -65,21 +184,26 @@ public record Voice(TimedFrequency[] frequencies) {
             TimedFrequency[] tfs = new TimedFrequency[pitches.size()];
             int i = 0;
             for (final var tp : pitches) {
-                tfs[i++] = tp.toTimedFrequency();
+                tfs[i++] = Timed.toFrequency(tp);
             }
             return new Voice(tfs);
         }
 
-        public static Builder of(List<TimedPitch> pitches) {
+        public static Builder of(List<Timed<Pitch>> pitches) {
             return new Builder(pitches);
         }
 
-        public static Builder of(TimedPitch[] pitches) {
+        public static Builder of(Timed<Pitch>[] pitches) {
             return new Builder(Arrays.asList(pitches));
+        }
+
+        @Override
+        public Voice get() {
+            return build();
         }
     }
 
-    public static final class FrequencyBuilder {
+    public static final class FrequencyBuilder implements Supplier<Voice> {
         private ArrayList<TimedFrequency> frequencies;
         private static final TimedFrequency[] TF_ARRAY = new TimedFrequency[0];
 
@@ -114,6 +238,21 @@ public record Voice(TimedFrequency[] frequencies) {
             return this;
         }
 
+        public FrequencyBuilder scaled(Fraction scalar, TimedFrequency frequency) {
+            frequencies.add(new TimedFrequency(frequency.frequency(), frequency.duration().multiply(scalar)));
+            return this;
+        }
+
+        public FrequencyBuilder scaled(Fraction scalar, TimedFrequency[] frequencies) {
+            this.frequencies.addAll(Arrays.stream(frequencies).map(p -> new TimedFrequency(p.frequency(), p.duration().multiply(scalar))).toList());
+            return this;
+        }
+
+        public FrequencyBuilder scaled(Fraction scalar, List<TimedFrequency> frequencies) {
+            this.frequencies.addAll(frequencies.stream().map(p -> new TimedFrequency(p.frequency(), p.duration().multiply(scalar))).toList());
+            return this;
+        }
+
         public Voice build() {
             return new Voice(frequencies.toArray(TF_ARRAY));
         }
@@ -124,6 +263,11 @@ public record Voice(TimedFrequency[] frequencies) {
 
         public static FrequencyBuilder of(TimedFrequency[] frequencies) {
             return new FrequencyBuilder(Arrays.asList(frequencies));
+        }
+
+        @Override
+        public Voice get() {
+            return build();
         }
     }
 }
